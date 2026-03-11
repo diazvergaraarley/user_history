2cf082f7dd937678dc9f1ee8a2c17f88a8dd9b4b
diazvergaraarley <diazvergara.arley@gmail.com>
2026-03-10 20:43:11 -0500

    Added lists/dictionaries exercises

---
+++ lista.py
@@ -0,0 +1,47 @@
+#Ejercicios de listas y diccionarios
+#1:
+    #fruits list:
+fruits= ["Banana", "Apple", "Watermelon", "Pineapple", "Strawberry"]
+print("fruits")
+    #a. add a fruit with append()
+fruits.append("Blackberry")
+print("a,", fruits)
+
+    #b. insert on position 2    
+fruits.insert(2, "Cucumber")
+print("b.", fruits)
+
+    #c. remove a fruit
+fruits.remove("Pineapple")
+print("c.", fruits)
+
+    #d. delete last element with pop()
+fruits.pop()
+print("d.", fruits)
+
+print("-")
+
+#2:
+    #numbers list
+numbers= [4, 2, 7, 2, 9, 1, 2]
+print("numbers: ", numbers)
+    #a. sort list
+sorted_numbers= sorted(numbers)
+print("a. Sorted lower to higher: ", sorted_numbers)
+
+    #b. reverse
+sorted_numbers.reverse()
+print("b. Sorted higher to lower: ", sorted_numbers)
+
+    #c. count
+counting= numbers.count(2)
+print("c. Counting number '2': ", counting)
+
+    #d. index
+positioning= numbers.index(7)
+print("d.Position of number '7': ", positioning)
+
+print("-")
+
+#3:
+
